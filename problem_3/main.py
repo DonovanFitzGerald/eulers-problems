e05589a75edf763b2e3435535bafbe9e27612182
seamus-oc <ociosais@tcd.ie>
2025-10-26 02:27:25 +0000

    problem 3 solved

--- problem_3/main.py
+++ problem_3/main.py
@@ -1,27 +1,26 @@
+import math
 primes = []
 primeFactors = []
-targetNumber = 13195
+targetNumber = 600851475143
 # 13195
 # 600851475143
 
 prevPrint = 0
 printInterval = 1000
-for i in range(3, targetNumber, 2):
-    
-    if i > prevPrint + printInterval - 1: 
+for i in range(3, int(math.sqrt(targetNumber)), 2):
+    if targetNumber % i == 0:
         print(i)
-        prevPrint = i
     
-    isPrime = True
-    for p in primes:
-        if i % p == 0:
-            isPrime = False
-            break
-        
-    if isPrime:
-        primes.append(i)
-        if targetNumber % i == 0:
-            primeFactors.append(i)
+        isPrime = True
+        for p in primes:
+            if i % p == 0:
+                isPrime = False
+                break
+            
+        if isPrime:
+            primes.append(i)
+            if targetNumber % i == 0:
+                primeFactors.append(i)
             
 print("Prime factor:", primeFactors)
 print("Largest prime factor:", primeFactors.pop())
