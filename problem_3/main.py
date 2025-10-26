fc36d7f8ceb6f0a0c6e51026b460bdaa5f8ca162
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-10-26 18:55:11 +0000

    Prime decomposition solution

--- problem_3/main.py
+++ problem_3/main.py
@@ -1,11 +1,14 @@
 import math
-primes = [2]
+primes = []
 primeFactors = []
-targetNumber = 13195
+targetNumber = 600851475143
 # 13195
 # 600851475143
 
 def checkIsPrime(num):
+    if num == 1 or num == 0: return False
+    if num in primes: return True
+    
     isPrime = True
     for p in primes:
             if num % p == 0:
@@ -17,32 +20,64 @@ def checkIsPrime(num):
     else:
         return False
     
+def printProgress(num, prevPrint, printInterval):
+    if prevPrint < num + printInterval:
+        print(num)
+        prevPrint += printInterval
+        
+    return prevPrint
+        
 
-prevPrint = 0
-printInterval = 1000
+def solveByDivisionTesting():
 
-for i in range(3, int(math.sqrt(targetNumber)), 2):
-    
-    # Progress printer
-    # if prevPrint < i + printInterval:
-    #     print(i)
-    #     prevPrint += printInterval
+    prevPrint = 0
+    for i in range(3, int(math.sqrt(targetNumber)), 2):
+        
+        # prevPrint = printProgress(i, prevPrint, 10000)
 
-    isPrime = checkIsPrime(i)
-    if isPrime:
-        if targetNumber % i == 0:
-            primeFactors.append(i)
-    
-if max(primeFactors) < int(math.sqrt(targetNumber)):
-    for p in primeFactors:
-        pairFactor = targetNumber / p
-        if not pairFactor.is_integer():
-            continue
-
-        isPrime = checkIsPrime(int(pairFactor))
+        isPrime = checkIsPrime(i)
         if isPrime:
-            primeFactors.append(int(pairFactor))
+            if targetNumber % i == 0:
+                primeFactors.append(i)
+        
+    if max(primeFactors) < int(math.sqrt(targetNumber)):
+        for p in primeFactors:
+            pairFactor = targetNumber / p
+            if not pairFactor.is_integer():
+                continue
+
+            isPrime = checkIsPrime(int(pairFactor))
+            if isPrime:
+                primeFactors.append(int(pairFactor))
+                
+
+def solveByDecomposition():
+    p = 1
+    currentTarget = targetNumber
+    prevPrint = 0
+    while p <= currentTarget:
+        
+        # prevPrint = printProgress(p, prevPrint, 1000)
+        
+        if not checkIsPrime(p):
+            p += 1
+            continue
+        
+        divisionResult = currentTarget / p
+        if divisionResult.is_integer():
+            divisionResult = int(divisionResult)
             
+            primeFactors.append(p)
+            currentTarget = divisionResult
+                
+            continue
+        
+        p += 1
+
+solveByDecomposition()
+# solveByDivisionTesting()
+            
+print("Target number:", targetNumber)
 # print("Primes:", primes)
 print("Prime factors:", primeFactors)
 print("Largest prime factor:", primeFactors.pop())
