13959c1c97ac7859a0c62ecacc5b6d7ff619d614
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-10-26 16:28:27 +0000

    Fixed solution for larger primes

--- problem_3/main.py
+++ problem_3/main.py
@@ -1,23 +1,48 @@
 import math
-primes = []
+primes = [2]
 primeFactors = []
-targetNumber = 600851475143
+targetNumber = 13195
 # 13195
 # 600851475143
 
-prevPrint = 0
-printInterval = 1000
-for i in range(3, int(math.sqrt(targetNumber)), 2):
-    if targetNumber % i == 0:
-        isPrime = True
-        for p in primes:
-            if i % p == 0:
+def checkIsPrime(num):
+    isPrime = True
+    for p in primes:
+            if num % p == 0:
                 isPrime = False
                 break
-            
-        if isPrime:
-            primes.append(i)
+    if isPrime:
+        primes.append(num)
+        return True
+    else:
+        return False
+    
+
+prevPrint = 0
+printInterval = 1000
+
+for i in range(3, int(math.sqrt(targetNumber)), 2):
+    
+    # Progress printer
+    # if prevPrint < i + printInterval:
+    #     print(i)
+    #     prevPrint += printInterval
+
+    isPrime = checkIsPrime(i)
+    if isPrime:
+        if targetNumber % i == 0:
             primeFactors.append(i)
+    
+if max(primeFactors) < int(math.sqrt(targetNumber)):
+    for p in primeFactors:
+        pairFactor = targetNumber / p
+        if not pairFactor.is_integer():
+            continue
+
+        isPrime = checkIsPrime(int(pairFactor))
+        if isPrime:
+            primeFactors.append(int(pairFactor))
             
+# print("Primes:", primes)
 print("Prime factors:", primeFactors)
 print("Largest prime factor:", primeFactors.pop())
