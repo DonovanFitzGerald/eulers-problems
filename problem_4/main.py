87c027e7dad99af31d009d3d00c628689ba81c33
seamus-oc <ociosais@tcd.ie>
2025-10-26 02:31:00 +0000

    problem 4 solved

--- problem_4/main.py
+++ problem_4/main.py
@@ -6,4 +6,12 @@ def isPalindrome(number):
             return False
     return True
 
-
+largestPalindrome = 0
+for i in range (100, 999):
+    for j in range (100, 999):
+        currentNumber = i*j
+        if isPalindrome(currentNumber) and currentNumber > largestPalindrome:
+            largestPalindrome = currentNumber
+            
+            
+print(largestPalindrome)
