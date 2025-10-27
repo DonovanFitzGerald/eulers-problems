226919e47b078be5bb4204f37063a9311323cc9a
seamus-oc <ociosais@tcd.ie>
2025-10-27 22:57:46 +0000

    problem 12 solved

--- problem_12/main.py
+++ problem_12/main.py
@@ -1,12 +1,28 @@
+import math
+
+def findDivisors(num):
+    divisors = []
+    sqrRoot = int(math.sqrt(num))
+    for divisor in range(1, sqrRoot + 1):
+        if num % divisor == 0:
+            divisors.append(divisor)
+            if divisor != sqrRoot:
+                divisors.append(num/divisor)
+    return divisors
+
+
+runningTotal = 0
+finished = False
+n = 1
+while not finished:
+    runningTotal += n
+    n += 1
+    
+    if len(findDivisors(runningTotal)) > 500:
+        finished = True
+    
+print(runningTotal)
+    
 
 
 
-
-def elipticCurvePrimeTest(num):
-    if num < 2: return False
-    for i in range(2, int(math.sqrt(num))+1):
-        if num % i == 0:
-            return False
-    print(num)
-    primes.append(num)
-    return True  
