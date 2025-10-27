73587c5304f8b68cef0659f61752aa26da49e897
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-10-27 02:19:13 +0000

    Two additional primality tests

--- problem_10/main.py
+++ problem_10/main.py
@@ -1,7 +1,9 @@
+import math
+
 primes = []
 
-def checkIsPrime(num):
-    if num == 1 or num == 0: return False
+def divisionPrimeTest(num):
+    if num < 2: return False
     for p in primes:
             if num % p == 0:
                 return False
@@ -9,6 +11,27 @@ def checkIsPrime(num):
     print(num)
     primes.append(num)
     return True
+
+def fermatsPrimeTest(num):
+    if num < 2: return False
+    for i in range(1,num):
+        isValid = ((i ** num) - i) % num == 0
+        if not isValid:
+            return False
+    print(num)
+    primes.append(num)
+    return False
+
+def elipticCurvePrimeTest(num):
+    if num < 2: return False
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    print(num)
+    primes.append(num)
+    return False    
+    
+    
     
 def printProgress(num, prevPrint, printInterval):
     if prevPrint < num + printInterval:
@@ -18,6 +41,6 @@ def printProgress(num, prevPrint, printInterval):
     return prevPrint
 
 for i in range(1,2000000):
-    checkIsPrime(i)
+    elipticCurvePrimeTest(i)
         
 print(sum(primes))
