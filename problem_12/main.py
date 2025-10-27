e02f2a152f1b32f4dc03553fbda4a35b12c3806f
seamus-oc <ociosais@tcd.ie>
2025-10-27 22:39:53 +0000

    problem 12 init

---
+++ problem_12/main.py
@@ -0,0 +1,12 @@
+
+
+
+
+def elipticCurvePrimeTest(num):
+    if num < 2: return False
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    print(num)
+    primes.append(num)
+    return True  
